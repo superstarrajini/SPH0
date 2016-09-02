a2699daa8847ce89fe30de8bd448a3d7cd8e5c89
Waftrue <waftrue@gmail.com>
2016-09-02 07:30:20 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,23 +1,51 @@
+<html>
+<head>
+<meta http-equiv="author" content="The Alchemist"/>
+<title>
+The Alchemist Fake Mailer
+</title>
+</head>
+<body background="http://fc00.deviantart.net/fs70/i/2011/324/9/2/black___purple_textures_by_paralyzinglove-d4gscvx.jpg">
+<p align=center>
+<img src="http://i67.photobucket.com/albums/h298/bcfcrule11/theAlchemist.png" alt="The Alchemist Fake Mailer" /></p>
+<form name="fakemail" action="<?php $PHP_SELF; ?>" method="POST">
+<p><label for="fname"><b><font size="5" color=c0c0c0>From name :</b></font></label><br>
+<input name="fname" id="fname" type="text" class="formbox" /><br></p>
+<p><label for="femail"><font size="5" color=c0c0c0>From email id :</font></label><br>
+<input name="femail" id="femail" type="text" class="formbox" /><br></p>
+<p><label for="to"><font size="5" color=c0c0c0>To :</font></label><br>
+<input name="to" id="to" type="text" class="formbox"/><br></p>
+<p><label for="subject"><font size="5" color=c0c0c0>Subject :</font></label><br>
+<input name="subject" id="subject" type="text" class="formbox"/><br></p>
+<p><label for="message"><font size="5" color=c0c0c0>Message :</font></label><br>
+<textarea name="message" id="message" cols="60" rows="8"></textarea></p>
+<p><input name="submit" id="submit" type="submit" value="Send!!" /></p></form>
 <?php
-
-//Stewie's IP Grabber
-
-//Variables
-
-$protocol = $_SERVER['SERVER_PROTOCOL'];
-$ip = $_SERVER['REMOTE_ADDR'];
-$port = $_SERVER['REMOTE_PORT'];
-$agent = $_SERVER['HTTP_USER_AGENT'];
-$ref = $_SERVER['HTTP_REFERER'];
-$hostname = gethostbyaddr($_SERVER['REMOTE_ADDR']);
-
-//Print IP, Hostname, Port Number, User Agent and Referer To Log.TXT
-
-$fh = fopen('log.txt', 'a'); 
-fwrite($fh, 'IP Address: '."".$ip ."\n");
-fwrite($fh, 'Hostname: '."".$hostname ."\n");
-fwrite($fh, 'Port Number: '."".$port ."\n");
-fwrite($fh, 'User Agent: '."".$agent ."\n");
-fwrite($fh, 'HTTP Referer: '."".$ref ."\n\n");
-fclose($fh);
+//Fake mailer code created by The Alchemist
+function send_email($to=null,$subject=null,$from_name=null,$from_mail=null,$mail_content=null,$replyto=null)
+{
+    $headers = "From: \"".$from_name."\" <".$from_mail.">\r\nReply-To: ".$replyto."\r\n";//here's the main part
+    if(@mail($to,$subject,$mail_content,$headers))
+    {
+        $mail_send_result="<p><font size=4 color=#c0c0c0>Email successfully sent to $to.!!</font></p>";//If mail gets sent successfully
+    }
+    else
+    {
+        $mail_send_result="<p><font size=4 color=#c0c0c0>Email NOT sent to $to.</font></p>";//If mail does not get sent
+    }
+    return $mail_send_result;
+}
+if(isset($_POST['to']) && isset($_POST['fname']) && isset($_POST['femail']) 
+&& isset($_POST['message']) && isset($_POST['subject']) && isset($_POST['submit']))
+{
+    $from_name=$_POST['fname'];
+    $from_mail=$_POST['femail'];
+    $mail_content=$_POST['message'];
+    $to=$_POST['to'];
+    $subject=$_POST['subject'];
+    $replyto=$_POST['femail'];
+    echo send_email($to,$subject,$from_name,$from_mail,$mail_content,$replyto);
+}
 ?>
+</body>
+</html> 
