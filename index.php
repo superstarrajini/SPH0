be088aed1824c473a6c087811c5c2e71f4a955d3
Waftrue <waftrue@gmail.com>
2016-09-05 21:08:25 +0300

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,8 @@
   <link rel="stylesheet" type="text/css" href="http://waftrue.comxa.com/style.css">
 <meta http-equiv="refresh" content="10;url=http://104.155.47.57/1/" />
 
+
+<link async href="http://fonts.googleapis.com/css?family=Chau%20Philomene%20One" data-generated="http://enjoycss.com" rel="stylesheet" type="text/css"/>
 </html>
 <?php
 header('Content-Type: text/html');
@@ -14,7 +16,22 @@ $clientip = $_SERVER['REMOTE_ADDR'];
 $api = file_get_contents("http://freegeoip.net/xml/{$clientip}");
 
 
-echo $api;
-
 ?>
+<div class="anaglyph"><?php echo $api; ?></div>
+<style>
+  
+  .anaglyph {
+  -webkit-box-sizing: content-box;
+  -moz-box-sizing: content-box;
+  box-sizing: content-box;
+  border: none;
+  font: normal 50px/normal "Chau Philomene One", Helvetica, sans-serif;
+  color: rgb(51, 51, 51);
+  text-align: center;
+  -o-text-overflow: clip;
+  text-overflow: clip;
+  letter-spacing: 3px;
+  text-shadow: -3px 0 1px rgb(30,242,241) , 3px 0 1px rgb(246,5,10) ;
+}
 
+</style>
