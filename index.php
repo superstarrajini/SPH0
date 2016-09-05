f50417b601c4eaf2b9eef5237f316edb242bc5bc
Waftrue <waftrue@gmail.com>
2016-09-05 07:40:35 +0300

    Update index.php

--- index.php
+++ index.php
@@ -7,8 +7,7 @@ $clientip = $_SERVER['REMOTE_ADDR'];
 $api1 = file_get_contents("http://freegeoip.net/xml/{$clientip}");
 
 
-$movies = new SimpleXMLElement($xmlstr);
 
-echo $movies->movie[0]->IP;
+echo ($api);
 
 ?>
