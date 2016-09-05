4a4235968a4bdbb866262ccc8c855e57bf44f41f
Waftrue <waftrue@gmail.com>
2016-09-05 08:11:05 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 
 <?php
-header('Content-Type: text/xml');
+header('Content-Type: text/html');
 
 
 $clientip = $_SERVER['REMOTE_ADDR'];
@@ -9,6 +9,6 @@ $clientip = $_SERVER['REMOTE_ADDR'];
 $api = file_get_contents("http://freegeoip.net/xml/{$clientip}");
 
 
-echo $api->Response[IP];
+echo $api->IP;
 ?>
 
